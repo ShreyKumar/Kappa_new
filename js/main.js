7d6e9aba37c496082e25ca808815a79e17fcf971
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2016-09-25 04:32:32 -0400

    emailjs works and added second get request

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ var api_key = "RGAPI-D8DF6459-E751-4C65-A84D-970183617834";
 var request_url;
 
 $(function(){
+  /* hide error msg */
+  $(".errormsg").hide();
   /* EmailJS report bug */
   emailjs.init("user_eL77breGlka8Ihde56BXT");
   $("#bugreport").click(function(){
+
     emailjs.send("default_service","summoner_name", {}).then(function(){
-        $(".reportmsg").show();
+        $("reportmsg").text("Reported. Thank you.");
     }, function(){
-        $(".reportmsg").show();
-        $(".reportmsg").text("Unable to send. :( Please try again later.")
+        $(".reportmsg").text("Unable to send. :( Please try again later.");
     });
   })
 
@@ -26,8 +28,17 @@ $(function(){
         request_url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summoner_name +"?api_key="+api_key;
         // make get request
         $.get(request_url, function(data){
-        }).fail(function(){
+          var summoner = data[Object.keys(data)[0]];
+          request_url = "https://na.api.pvp.net/championmastery/location/NA1/player/" + summoner.id + "/champions?api_key=" + api_key;
+          $.get(request_url, function(data){
+            console.log(data);
+          }).fail(function(){
+            $(".errormsg").show();
+            console.log("woops");
+          })
 
+        }).fail(function(){
+          $(".errormsg").show();
         })
       }
     }
