36063fb7b8f9598ece48aae42c26b423023cf97d
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2016-09-25 04:11:57 -0400

    added external libraries and champion pictures

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/* Global vars */
+var api_key = "RGAPI-D8DF6459-E751-4C65-A84D-970183617834";
+var request_url;
+
+$(function(){
+  /* EmailJS report bug */
+  emailjs.init("user_eL77breGlka8Ihde56BXT");
+  $("#bugreport").click(function(){
+    emailjs.send("default_service","summoner_name", {}).then(function(){
+        $(".reportmsg").show();
+    }, function(){
+        $(".reportmsg").show();
+        $(".reportmsg").text("Unable to send. :( Please try again later.")
+    });
+  })
+
+
+  $("#name").keyup(function(e){
+    if(e.keyCode == 13){
+      //pressed enter and not empty
+      if($("#name").val() == ""){
+        $("#name").addClass("error");
+      } else {
+        $("#name").removeClass("error");
+        var summoner_name = $("#name").val();
+        request_url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summoner_name +"?api_key="+api_key;
+        // make get request
+        $.get(request_url, function(data){
+        }).fail(function(){
+
+        })
+      }
+    }
+  })
+});
